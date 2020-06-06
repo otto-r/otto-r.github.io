cb1d358503fd0c2deb082a635dd4938ed6922482
Otto Rinaldo <otto.rinaldo@gmail.com>
2020-06-06 09:20:40 +0200

    init

---
+++ precache-manifest.70d7fe51afd84f721a039867fa5660f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c0761a7cc531c9c4e4",
+    "url": "/css/app.cfeba930.css"
+  },
+  {
+    "revision": "62922fbcb89df8371572ff8bc2429875",
+    "url": "/img/bird.62922fbc.jpg"
+  },
+  {
+    "revision": "0e9eb742f0855561c30a06525904ae17",
+    "url": "/img/bus.0e9eb742.jpg"
+  },
+  {
+    "revision": "70453524214636d72ae63034b9c76edb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6837a12bf06e555938d",
+    "url": "/js/about.4029c874.js"
+  },
+  {
+    "revision": "a8c0761a7cc531c9c4e4",
+    "url": "/js/app.94235b94.js"
+  },
+  {
+    "revision": "d6162612300f65a0b394",
+    "url": "/js/chunk-vendors.8f1306cf.js"
+  },
+  {
+    "revision": "60817f553ce8324bc847a5b778fffada",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
